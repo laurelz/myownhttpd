b1b6f3cb2b20522e5caea687dad14da60b82b795
laurelz <laurelzhong@hotmail.com>
2015-04-23 21:20:29 +0800

    Create createc.sh

---
+++ createc.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "I am a shell to create some clients"
+if [ $# -eq 0 ]; then
+	echo "no arguments"
+fi
+num=1;
+if [ $# -eq 1 ]; then
+	num=$@;
+fi
+echo $num;
+count=0;
+while [ $count -lt $num ]
+do
+    echo $count
+	#create some terminals, each terminal execute client once
+    gnome-terminal -t "terminal$count" -x bash -c "sh ./clientexec.sh;"
+	count=$(($count+1));
+done
